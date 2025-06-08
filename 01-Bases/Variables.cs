8b9300f4aece2f897f0bc7cd8a4db7fe6e797b39
TuNombre <johantirado24@gmail.com>
2025-06-07 19:20:31 -0500

    Variables

---
+++ 01-Bases/Variables.cs
@@ -0,0 +1,13 @@
+partial class Program
+{
+    static void Variables()
+    {
+        Console.WriteLine("Variables in C#");
+
+        int edad = 25;
+        string nombre = "Johan";
+        bool esAnalista = true;
+        Console.WriteLine($"Nombre: {nombre}, Edad: {edad}, Es Analista: {esAnalista}");
+                
+    }
+}
